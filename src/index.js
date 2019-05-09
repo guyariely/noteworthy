43af8cd6faf9ddd1b8dd731f6d301560d4f915dd
Guy Arieli <guyarieli1997@gmail.com>
2019-05-09 10:24:20 +0300

    adding Quill support for rich text editing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './styles/styles.scss';
+import 'normalize.css/normalize.css';
+import Editor from './components/Noteworthy.2';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Editor />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
